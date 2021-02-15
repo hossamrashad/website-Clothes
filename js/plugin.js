c0aff73b555d1337752ed6fcd2c6a1294a61f6f4
Hossam Rashad <bibo2010508@yahoo.com>
2021-02-15 02:46:50 +0200

    i Will Bak

--- js/plugin.js
+++ js/plugin.js
@@ -1,2 +1,8 @@
 /* jslint plusplus: true, evil:true */
 /* globel $, jQuery, console, document, window, alert, prompt, confirm */
+
+$(document).ready(function () {
+
+    $('body').css('paddingTop', $('.navbarPadding').innerHeight());
+
+});
